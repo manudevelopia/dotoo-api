4add29ed11a166a1307c1673fef82a17a0ac21fb
manudevelopia <manugarciarodriguez@gmail.com>
2024-09-01 11:49:02 +0200

    implement update

--- app/src/main/java/info/developia/Launcher.java
+++ app/src/main/java/info/developia/Launcher.java
@@ -4,7 +4,6 @@ package info.developia;
 import info.developia.dotoo.api.persistence.Persistence;
 import info.developia.dotoo.api.server.GraphqlService;
 import info.developia.dotoo.api.service.TaskService;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class Launcher {
     public static final Persistence persistence = new Persistence("info.developia.dotoo.api.repository.mapper");
