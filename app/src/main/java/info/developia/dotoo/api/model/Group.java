eba67894bb7bef0db6d465b3b4f97ab1a258dd3f
manudevelopia <manugarciarodriguez@gmail.com>
2024-10-10 11:25:29 +0200

    wip

--- app/src/main/java/info/developia/dotoo/api/model/Group.java
+++ app/src/main/java/info/developia/dotoo/api/model/Group.java
@@ -1,2 +1,10 @@
-package info.developia.dotoo.api.model;public record List() {
+package info.developia.dotoo.api.model;
+
+import java.util.List;
+
+public record Group(
+        String id,
+        String title,
+        List<Task> tasks
+) {
 }
